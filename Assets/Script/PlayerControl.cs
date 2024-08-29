397e6f18c299ba94f2da2f7e21ecf32e367cc9de
KasiarAffan <kaisaraffan1@gmail.com>
2024-08-29 12:02:50 +0700

    fix movement[Ir]

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Windows;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour
@@ -15,7 +13,7 @@ public class PlayerControl : MonoBehaviour
     private Vector3 _velocity;
     private float _currentVelocity;
     [SerializeField] private float smoothTime = 0.05f;
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 5f;
     [SerializeField] private float gravity = -9.81f;
 
     private void Awake()
@@ -26,33 +24,46 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
-        if (_input.sqrMagnitude == 0)
+        if (_input.sqrMagnitude > 0)
+        {
+            animator.SetBool("Moving", true);
+
+            Vector3 forward = Camera.main.transform.forward;
+            Vector3 right = Camera.main.transform.right;
+
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+
+            _direction = forward * _input.y + right * _input.x;
+
+            var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+            _characterController.Move(_direction * speed * Time.deltaTime);
+
+            Debug.Log($"Moving in direction: {_direction}");
+        }
+        else
         {
             animator.SetBool("Moving", false);
-            return;
         }
 
-        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
-        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-
-        _characterController.Move(_direction * speed * Time.deltaTime);
-
-        // Apply gravity
         _velocity.y += gravity * Time.deltaTime;
         _characterController.Move(_velocity * Time.deltaTime);
 
-        // Reset the velocity if the character is grounded
         if (_characterController.isGrounded && _velocity.y < 0)
         {
-            _velocity.y = -15f;
+            _velocity.y = -2f;
         }
-
-        animator.SetBool("Moving", true);
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        _direction = new Vector3(_input.x, 0.0f, _input.y);
+
+        Debug.Log($"Input: {_input}, Direction: {_direction}");
     }
 }
